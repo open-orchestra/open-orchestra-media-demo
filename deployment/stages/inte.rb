f10db5b4c70f6482476d212915b11136c923cacd
Nicolas Thal <nico.th4l@gmail.com>
2015-08-04 10:19:41 +0200

    add deployment files

---
+++ deployment/stages/inte.rb
@@ -0,0 +1,3 @@
+server 'open_orchestra_media_inte', roles: %w{web app db}
+set :repo_url, 'git@github.com:open-orchestra/open-orchestra-media-demo.git'
+set :deploy_to, '/var/www/media-open-orchestra'
