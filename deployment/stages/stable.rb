c0b08e76648bdd0da95bb84dd9649a8cc3ae1a2f
Amaury Lavieille <amaury.lavieille@businessdecision.com>
2015-10-16 15:17:26 +0200

    add environment stable media

---
+++ deployment/stages/stable.rb
@@ -0,0 +1,4 @@
+server 'open_orchestra_media_stable', roles: %w{web app db env}
+set :repo_url, 'git@github.com:open-orchestra/open-orchestra-media-demo.git'
+set :branch, '1.0'
+set :application, 'OpenOrchestraMediaStable'
