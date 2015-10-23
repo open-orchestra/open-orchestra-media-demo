979bd955ab853957c64cbc8dfc060f69ada304e9
Nicolas Thal <nico.th4l@gmail.com>
2015-10-23 14:15:28 +0200

    activate the model bundle only for tests

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -29,6 +29,9 @@ class AppKernel extends Kernel
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
             $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
+        }
+
+        if (in_array($this->getEnvironment(), array('test'))) {
             $bundles[] = new OpenOrchestra\ModelBundle\OpenOrchestraModelBundle();
             $bundles[] = new Solution\MongoAggregationBundle\SolutionMongoAggregationBundle();
         }
