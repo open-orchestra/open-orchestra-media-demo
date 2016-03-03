5f60af2d9fe6006a1da8f1c30aba247bcfa97938
Amaury Lavieille <amaury.lavieille@businessdecision.com>
2016-03-03 09:56:57 +0100

    add caspitrano task to update vendor

--- deployment/stages/inte.rb
+++ deployment/stages/inte.rb
@@ -1,4 +1,7 @@
 server 'open_orchestra_media_inte', roles: %w{web app db env}
 set :repo_url, 'git@github.com:open-orchestra/open-orchestra-media-demo.git'
 set :deploy_to, '/var/www/media-open-orchestra'
+set :branch, 'master'
 set :application, 'OpenOrchestraMedia'
+set :update_dir, 'update-vendor-media_inte'
+set :git_project_dir, 'open-orchestra-media-demo'
