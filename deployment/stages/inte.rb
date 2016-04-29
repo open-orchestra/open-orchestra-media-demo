b35e0b6aa627407ae2f63f60c6ca278673119ca9
Amaury Lavieille <amaury.lavieille@businessdecision.com>
2016-04-29 12:32:24 +0200

    update deploy 1.0 inte

--- deployment/stages/inte.rb
+++ deployment/stages/inte.rb
@@ -1,3 +1,3 @@
-server 'open_orchestra_media_inte', roles: %w{web app db env}
+server 'open_orchestra_media_inte_1-0', roles: %w{web app db env}
 set :repo_url, 'git@github.com:open-orchestra/open-orchestra-media-demo.git'
 set :deploy_to, '/var/www/media-open-orchestra'
