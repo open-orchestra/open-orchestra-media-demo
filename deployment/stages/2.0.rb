f4de92376372a0dba4482efb1fc358244d4975fd
Amaury Lavieille <amaury.lavieille@businessdecision.com>
2016-10-07 16:38:58 +0200

    update deploy 2.0

--- deployment/stages/2.0.rb
+++ deployment/stages/2.0.rb
@@ -1,4 +1,4 @@
-server 'open_orchestra_media_inte_1-2', roles: %w{web app db env}
+server 'open_orchestra_media_inte_2-0', roles: %w{web app db env}
 set :repo_url, 'https://github.com/open-orchestra/open-orchestra-media-demo.git'
 set :deploy_to, '/home/wwwroot/openorchestra/media'
 set :branch, 'master'
