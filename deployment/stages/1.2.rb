6938c1e18f8c5147caac973d805afb9d058bd627
Amaury Lavieille <amaury.lavieille@businessdecision.com>
2016-10-07 16:15:52 +0200

    update deploy 1.2

--- deployment/stages/1.2.rb
+++ deployment/stages/1.2.rb
@@ -1,7 +1,7 @@
 server 'open_orchestra_media_inte_1-2', roles: %w{web app db env}
 set :repo_url, 'https://github.com/open-orchestra/open-orchestra-media-demo.git'
 set :deploy_to, '/home/wwwroot/openorchestra/media'
-set :branch, 'master'
+set :branch, '1.2'
 set :application, 'OpenOrchestraMedia'
 set :update_dir, 'update-vendor-media_inte'
 set :git_project_dir, 'open-orchestra-media-demo'
